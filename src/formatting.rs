1181a2733f8c5e4c3dd39369aa853f5a22f6ebdc
Jenya705 <51133999+Jenya705@users.noreply.github.com>
2022-07-09 20:31:44 +0200

    Updated identifiers and formatting

---
+++ src/formatting.rs
@@ -0,0 +1,144 @@
+use std::borrow::Cow;
+use std::cmp::Ordering;
+use std::fmt::{Display, Formatter};
+
+type HexColorInner<'a> = either::Either<(u8, u8, u8), Cow<'a, str>>;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Style {
+    Random,
+    Bold,
+    Strikethrough,
+    Underlined,
+    Italic,
+    Reset
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[serde(try_from = "String", into = "String")]
+pub struct HexColor<'a>(HexColorInner<'a>);
+
+#[derive(Debug, Clone, Copy, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum DefaultColor {
+    Black,
+    DarkBlue,
+    DarkGreen,
+    DarkCyan,
+    DarkRed,
+    Purple,
+    Gold,
+    Gray,
+    DarkGray,
+    Blue,
+    BrightGreen,
+    Cyan,
+    Red,
+    Pink,
+    Yellow,
+    White,
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[serde(untagged)]
+pub enum Color<'a> {
+    Default(DefaultColor),
+    Hex(HexColor<'a>),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, thiserror::Error)]
+pub enum HexColorError {
+    #[error("Hex value contains bad characters")]
+    HexValueContainsBadCharacters,
+    #[error("Hex value too long")]
+    HexValueTooLong,
+    #[error("Hex value too small")]
+    HexValueTooSmall,
+}
+
+impl<'a> HexColor<'a> {
+    const fn new(inner: HexColorInner<'a>) -> Self {
+        Self(inner)
+    }
+
+    const fn get(&self) -> &HexColorInner<'a> {
+        &self.0
+    }
+
+    pub const fn new_rgb(r: u8, g: u8, b: u8) -> Self {
+        Self::new(either::Either::Left((r, g, b)))
+    }
+
+    pub fn new_hex(hex: impl Into<Cow<'a, str>>) -> Result<Self, HexColorError> {
+        let hex = hex.into();
+        match hex.len().cmp(&7) {
+            Ordering::Less => Err(HexColorError::HexValueTooLong),
+            Ordering::Equal => Err(HexColorError::HexValueTooLong),
+            // Safety. The length is 7 so next will get first element, which is exist
+            Ordering::Greater => match unsafe { hex.chars().next().unwrap_unchecked() } == '#' &&
+                hex[1..=7].contains(|c: char| {
+                    match c {
+                        '0'..='9' | 'a'..='f' | 'A'..='F' => false,
+                        _ => true
+                    }
+                }) {
+                true => Err(HexColorError::HexValueContainsBadCharacters),
+                false => Ok(Self::new(HexColorInner::Right(hex)))
+            }
+        }
+    }
+
+    pub fn get_rgb(&self) -> (u8, u8, u8) {
+        match self.get() {
+            HexColorInner::Left((r, g, b)) => (*r, *g, *b),
+            // Safety. Guarantied by private constructors
+            HexColorInner::Right(str) => unsafe {
+                (
+                    u8::from_str_radix(&str[1..3], 16).unwrap_unchecked(),
+                    u8::from_str_radix(&str[3..5], 16).unwrap_unchecked(),
+                    u8::from_str_radix(&str[5..7], 16).unwrap_unchecked(),
+                )
+            }
+        }
+    }
+
+    pub fn get_hex(&'a self) -> Cow<'a, str> {
+        match self.get() {
+            HexColorInner::Left((r, g, b)) =>
+                Cow::Owned(format!("#{:02x}{:02x}{:02x}", r, g, b)),
+            HexColorInner::Right(str) => Cow::Borrowed(&str)
+        }
+    }
+}
+
+impl Display for HexColor<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.get_hex())
+    }
+}
+
+impl TryFrom<String> for HexColor<'_> {
+    type Error = HexColorError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        HexColor::new_hex(value)
+    }
+}
+
+impl From<HexColor<'_>> for String {
+    fn from(color: HexColor<'_>) -> Self {
+        color.to_string()
+    }
+}
+
+impl From<DefaultColor> for Color<'_> {
+    fn from(default_color: DefaultColor) -> Self {
+        Color::Default(default_color)
+    }
+}
+
+impl<'a> From<HexColor<'a>> for Color<'a> {
+    fn from(hex_color: HexColor<'a>) -> Self {
+        Color::Hex(hex_color)
+    }
+}
--- src/identifier.rs
+++ src/identifier.rs
@@ -1,14 +1,28 @@
 use std::borrow::Cow;
 use std::fmt::{Display, Formatter};
+use std::str::pattern::{Pattern, Searcher};
 
 #[derive(Clone, Debug)]
-enum IdentifierInner<'a> {
-    Fulled(&'a str),
-    Partial(&'a str, &'a str),
+pub enum IdentifierInner<'a> {
+    Fulled(Cow<'a, str>),
+    Partial(Cow<'a, str>, Cow<'a, str>),
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+#[serde(try_from = "String", into = "String")]
 pub struct Identifier<'a>(IdentifierInner<'a>);
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, thiserror::Error)]
+pub enum IdentifierError {
+    #[error("Value contains double dot")]
+    ValueContainsDoubleDot,
+    #[error("Key contains double dot")]
+    KeyContainsDoubleDot,
+    #[error("Fulled contains more than one double dot")]
+    FulledContainsMoreThanOneDoubleDot,
+    #[error("Fulled contains no double dots")]
+    FulledContainsNoDoubleDot,
+}
 
 impl<'a> Identifier<'a> {
     const fn new(inner: IdentifierInner<'a>) -> Self {
@@ -19,52 +33,99 @@ impl<'a> Identifier<'a> {
         &self.0
     }
 
-    /// If value is full identifier then new_fulled function with value argument will be used
+    pub fn into_inner(self) -> IdentifierInner<'a> {
+        self.0
+    }
+
+    pub const unsafe fn from_inner_unchecked(inner: IdentifierInner<'a>) -> Self {
+        Self::new(inner)
+    }
+
+    pub fn from_inner(inner: IdentifierInner<'a>) -> Result<Self, IdentifierError> {
+        match inner {
+            IdentifierInner::Fulled(fulled) => Self::new_fulled(fulled),
+            IdentifierInner::Partial(key, value) => Self::new_partial(key, value),
+        }
+    }
+
+    /// If value is full identifier then new_fulled function with value argument will be used.
     /// If value is not full identifier then new_partial with default_key and value arguments will be used
-    pub fn new_with_default(value: &'a str, default_key: &'a str) -> Option<Self> {
-        match value.find(":") {
-            // Safety. as full contains ':' then this function will always return Some
-            Some(index) => match unsafe { value.rfind(":").unwrap_unchecked() } == index {
-                true => Some(Self::new(IdentifierInner::Fulled(value))),
-                false => None,
+    pub fn new_with_default(value: impl Into<Cow<'a, str>>, default_key: impl Into<Cow<'a, str>>) -> Result<Self, IdentifierError> {
+        let value = value.into();
+        let default_key = default_key.into();
+        let mut searcher = ':'.into_searcher(&value);
+        match searcher.next_match() {
+            Some(_) => match searcher.next_match() {
+                Some(_) => Err(IdentifierError::FulledContainsMoreThanOneDoubleDot),
+                None => Ok(Self::new(IdentifierInner::Fulled(value))),
             },
-            None => match default_key.contains(":") {
-                true => None,
-                false => Some(Self::new(IdentifierInner::Partial(default_key, value)))
+            None => match default_key.contains(':') {
+                true => Err(IdentifierError::KeyContainsDoubleDot),
+                false => Ok(Self::new(IdentifierInner::Partial(default_key, value)))
             }
         }
     }
 
-    pub fn new_fulled(full: &'a str) -> Option<Self> {
-        match full.find(":") {
-            // Safety. as full contains ':' then this function will always return Some
-            Some(index) => match unsafe { full.rfind(":").unwrap_unchecked() } == index {
-                true => Some(Self::new(IdentifierInner::Fulled(full))),
-                false => None,
+    pub fn new_fulled(full: impl Into<Cow<'a, str>>) -> Result<Self, IdentifierError> {
+        let full = full.into();
+        let mut searcher = ':'.into_searcher(&full);
+        match searcher.next_match() {
+            Some(_) => match searcher.next_match() {
+                Some(_) => Err(IdentifierError::FulledContainsMoreThanOneDoubleDot),
+                None => Ok(Self::new(IdentifierInner::Fulled(full))),
             },
-            None => None,
+            None => Err(IdentifierError::FulledContainsNoDoubleDot),
         }
     }
 
-    pub fn new_partial(key: &'a str, value: &'a str) -> Option<Self> {
-        match key.contains(":") || value.contains(":") {
-            true => None,
-            false => Some(Self::new(IdentifierInner::Partial(key, value)))
+    pub fn new_partial(key: impl Into<Cow<'a, str>>, value: impl Into<Cow<'a, str>>) -> Result<Self, IdentifierError> {
+        let key = key.into();
+        let value = value.into();
+        match key.contains(':') {
+            true => Err(IdentifierError::KeyContainsDoubleDot),
+            false => match value.contains(':') {
+                true => Err(IdentifierError::ValueContainsDoubleDot),
+                false => Ok(Self::new(IdentifierInner::Partial(key, value)))
+            }
         }
     }
 
-    pub fn get_fulled(&self) -> Cow<'a, str> {
+    pub fn get_fulled(&'a self) -> Cow<'a, str> {
         match self.get_inner() {
-            IdentifierInner::Fulled(str) => Cow::Borrowed(str),
-            IdentifierInner::Partial(key, value) => Cow::Owned(format!("{}:{}", key, value))
+            IdentifierInner::Fulled(fulled) => Cow::Borrowed(&fulled),
+            IdentifierInner::Partial(key, value) =>
+                Cow::Owned(format!("{}:{}", key, value))
         }
     }
 
-    pub fn get_partial(&self) -> (&'a str, &'a str) {
+    pub fn get_partial(&'a self) -> (&'a str, &'a str) {
         match self.get_inner() {
-            IdentifierInner::Fulled(str) => {
-                let index = unsafe { str.find(":").unwrap_unchecked() };
-                (&str[0..index], &str[index + 1..str.len()])
+            IdentifierInner::Fulled(fulled) => {
+                // Safety. guarantied by private constructors
+                let index = unsafe { fulled.find(":").unwrap_unchecked() };
+                (&fulled[0..index], &fulled[index + 1..fulled.len()])
+            }
+            IdentifierInner::Partial(key, value) => (&key, &value)
+        }
+    }
+
+    pub fn into_fulled(self) -> Cow<'a, str> {
+        match self.into_inner() {
+            IdentifierInner::Fulled(fulled) => fulled,
+            IdentifierInner::Partial(key, value) =>
+                Cow::Owned(format!("{}:{}", key, value))
+        }
+    }
+
+    pub fn into_partial(self) -> (Cow<'a, str>, Cow<'a, str>) {
+        match self.into_inner() {
+            IdentifierInner::Fulled(fulled) => {
+                // Safety. guarantied by private constructors
+                let index = unsafe { fulled.find(":").unwrap_unchecked() };
+                (
+                    Cow::Owned(fulled[0..index].to_owned()),
+                    Cow::Owned(fulled[index + 1..fulled.len()].to_owned())
+                )
             }
             IdentifierInner::Partial(key, value) => (key, value)
         }
@@ -104,5 +165,72 @@ impl PartialEq for Identifier<'_> {
 
     fn ne(&self, other: &Self) -> bool {
         !self.eq(other)
+    }
+}
+
+impl TryFrom<String> for Identifier<'_> {
+    type Error = IdentifierError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        Self::new_fulled(value)
+    }
+}
+
+impl From<Identifier<'_>> for String {
+    fn from(identifier: Identifier<'_>) -> Self {
+        identifier.to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn identifier_constructor() {
+        {
+            let good_identifier = Identifier::new_fulled("minecraft:grass_block").unwrap();
+            assert_eq!(good_identifier.to_string(), "minecraft:grass_block");
+            assert_eq!(good_identifier.get_partial(), ("minecraft", "grass_block"));
+            assert_eq!(good_identifier.get_fulled(), Cow::Borrowed("minecraft:grass_block"));
+            assert_eq!(
+                good_identifier,
+                Identifier::from_inner(IdentifierInner::Fulled(Cow::Borrowed("minecraft:grass_block"))).unwrap()
+            );
+        }
+        {
+            assert_eq!(
+                Identifier::new_partial("minecraft", "grass_block"),
+                Identifier::new_fulled("minecraft:grass_block")
+            );
+        }
+        {
+            assert_eq!(
+                Identifier::new_fulled("minecraft:grass_block:"),
+                Err(IdentifierError::FulledContainsMoreThanOneDoubleDot)
+            );
+            assert_eq!(
+                Identifier::new_fulled("minecraft_grass_block"),
+                Err(IdentifierError::FulledContainsNoDoubleDot)
+            );
+            assert_eq!(
+                Identifier::new_partial("minecraft", "grass_block:"),
+                Err(IdentifierError::ValueContainsDoubleDot)
+            );
+            assert_eq!(
+                Identifier::new_partial("minecraft:", "grass_block"),
+                Err(IdentifierError::KeyContainsDoubleDot)
+            );
+        }
+        {
+            assert_eq!(
+                Identifier::new_with_default("grass_block", "minecraft"),
+                Identifier::new_fulled("minecraft:grass_block")
+            );
+            assert_eq!(
+                Identifier::new_with_default("other:grass_block", "minecraft"),
+                Identifier::new_fulled("other:grass_block")
+            );
+        }
     }
 }
